3932f89456ffe973cb0b2d231caeafe844bfe61f
inksne <debanyj23@gmail.com>
2024-11-01 21:56:02 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,159 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+from datetime import datetime
+
+from database.database import create_db_and_tables, get_async_session
+from models.models import Role, Post, User
+
+app = FastAPI(
+    title='forum'
+)
+
+
+#CORS
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
+    allow_headers=['Content-Type', 'Set-Cookie', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Origin', 'Authorization'],
+)
+
+#pydantic модели для restfulAPI
+class PostCreate(BaseModel):
+    author_id: int
+    description: str
+
+class RoleResponse(BaseModel):
+    id: int
+    name: str
+    permissions: dict
+
+class PostResponse(BaseModel):
+    id: int
+    deployed_at: datetime
+    author_id: int
+    description: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+    username: str
+    registered_at: datetime
+    role_id: int
+
+class UserUpdateUsername(BaseModel):
+    username: str
+
+#restfulAPI
+
+@app.on_event("startup")
+async def startup():
+    await create_db_and_tables()
+
+
+
+# @app.post("/roles/", response_model=dict)
+# async def create_role(name: str, permissions: dict = None, session: AsyncSession = Depends(get_async_session)):
+#     new_role = Role(name=name, permissions=permissions)
+#     session.add(new_role)
+#     await session.commit()
+#     await session.refresh(new_role)  # Обновить объект, чтобы получить id
+#     return {"id": new_role.id, "name": new_role.name, "permissions": new_role.permissions}
+
+
+
+@app.get("/roles/", response_model=list[dict])
+async def get_all_roles(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Role))
+    roles = result.scalars().all()
+    return [{"id": role.id, "name": role.name, "permissions": role.permissions} for role in roles]
+
+
+
+@app.get('/posts/', response_model=list[dict])
+async def get_all_posts(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post))
+    posts = result.scalars().all()
+    return [{'id': post.id, 'deployed_at': post.deployed_at, 'author_id': post.author_id, 'description': post.description} for post in posts]
+
+
+
+@app.post('/posts/', response_model=PostResponse)
+async def create_post(post: PostCreate, session: AsyncSession = Depends(get_async_session)):
+    new_post = Post(author_id=post.author_id, description=post.description)
+    session.add(new_post)
+    await session.commit()
+    await session.refresh(new_post)
+    return new_post
+
+
+
+@app.get('/users/{user_id}', response_model=UserResponse)
+async def get_user_by_id(user_id: int, session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(User).where(User.id == user_id))
+    user = result.scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+    return user
+
+
+
+@app.get('/users/', response_model=list[dict])
+async def get_all_users(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(User))
+    users = result.scalars().all()
+    return [
+        {'id': user.id,
+        'email': user.email,
+        'username': user.username,
+        'registered_at': user.registered_at,
+        'role_id': user.role_id} 
+        for user in users]
+
+
+
+@app.put('/users/{user_id}', response_model=UserResponse)
+async def change_username(user_id: int, user_update_username: UserUpdateUsername, session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(User).where(User.id == user_id))
+    user = result.scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+    user.username = user_update_username.username
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
+
+
+
+@app.get('/roles/{role_id}', response_model=RoleResponse)
+async def get_user_by_id(role_id: int, session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Role).where(Role.id == role_id))
+    role = result.scalar_one_or_none()
+    if role is None:
+        raise HTTPException(status_code=404, detail='Роль не найдена')
+    return role
+
+
+@app.delete('/users/{user_id}', response_model=str)
+async def delete_user_by_id(user_id: int, session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(User).where(User.id == user_id))
+    user = result.scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+    await session.delete(user)
+    await session.commit()
+    return 'Пользователь успешно удален.'
---
+++ models/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey, Column, JSON, Boolean
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.orm import DeclarativeBase
+
+Base: DeclarativeMeta = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
+
+class Role(Base):
+    __tablename__ = 'role'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    permissions = Column(JSON)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    hashed_password = Column(String(length=1024), nullable=False)
+    registered_at = Column(TIMESTAMP, server_default=func.now())
+    role_id = Column(Integer, ForeignKey(Role.id))
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    is_verified = Column(Boolean, default=False, nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    deployed_at = Column(TIMESTAMP, server_default=func.now())
+    author_id = Column(Integer, ForeignKey(User.id)) 
+    description = Column(String(length=1024), nullable=False)
