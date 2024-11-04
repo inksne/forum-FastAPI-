f511f4420d873652c01ee2e5d0650c0bfb1e0b02
inksne <debanyj23@gmail.com>
2024-11-04 21:59:36 +0300

    delete null comments

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from fastapi.middleware.cors import CORSMiddleware
-#
 
 from pydantic import BaseModel
 from datetime import datetime
