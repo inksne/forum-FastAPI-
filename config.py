e26956662b017e6ca3ef0a591c2d2f2efa85522c
inksne <debanyj23@gmail.com>
2024-11-12 19:53:38 +0300

    change time access token

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class AuthJWT(BaseModel):
     private_key_path: Path = Path("certs") / "jwt-private.pem"
     public_key_path: Path = Path("certs") / "jwt-public.pem"
     algorithm: str = "RS256"
-    access_token_expire_minutes: int = 15
+    access_token_expire_minutes: int = 120
     refresh_token_expire_days: int = 30
 
 
