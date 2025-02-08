5115b605571d74405cfdf3cc0d1369f3d216ca6b
inksne <134765647+inksne@users.noreply.github.com>
2025-02-08 09:25:44 +0300

    Delete lib directory

--- lib/config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
